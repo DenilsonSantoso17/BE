7c4ae2485f8cfb8e7bd9ff42c82f9134415012f9
DenilsonSantoso17 <57387034+DenilsonSantoso17@users.noreply.github.com>
2023-10-07 12:24:56 +0700

    update

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
+require('dotenv').config;
+
+const cors = require('cors');
 const express = require('express');
+
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use(cors()); //untuk mengaktifkan cors, agar API bisa diakses dari luar atau dari FrontEnd
+app.use(express.json()) //parse request body dari JSON ke Object
+app.use(express.urlencoded({ extended : true })) //parse request body dari x-www-form-urlencoded ke object 
 
 
 app.get('/', (req, res) => {
